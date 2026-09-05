f2ed2765b5cb630a46d0c0d1eeeec627fd381dd7
UIT25520922 <25520922@gm.uit.edu.vn>
2026-09-05 14:43:02 +0000

    feat: add Student class

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void display() {
+        System.out.println(id + " - " + name);
+    }
+}
